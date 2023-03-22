029cb97332468d96c25c2e4a597ef2f0c8aedbb0
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2023-03-22 17:13:38 +0200

    finishChange

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/FormContact/FormContact';
 import { Section } from 'components/Section/Section';
 import { RenderContacts } from 'components/RenderContacts/RenderContacts';
 import { Filter } from 'components/Filter/Filter';
 import { Container } from './App.styled';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts === null) {
+      return [];
+    }
+    return savedContacts;
+  });
   const [filter, setFilter] = useState('');
-  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const onHandleSubmit = ({ name, number }) => {
     const filterName = contacts.find(contact => contact.name === name);
@@ -33,7 +35,7 @@ export const App = () => {
       number: number,
     };
 
-    setContacts([contact, ...contacts]);
+    setContacts(prevContacts => [contact, ...prevContacts]);
   };
 
   const changeFilter = e => {
@@ -41,20 +43,10 @@ export const App = () => {
   };
 
   const deleteContacts = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
-
-  useEffect(() => {
-    if (isFirstRender.current) {
-      const parseContacts = JSON.parse(localStorage.getItem('contacts'));
-      if (parseContacts) {
-        setContacts(parseContacts);
-      }
-      isFirstRender.current = false;
-      return;
-    }
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   const normalizeFilter = filter.toLowerCase();
   const visibleContacts = contacts.filter(contact =>
